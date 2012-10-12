8384cf293da1893dd47b8617b50ba36eee851fc8
Dan Palmer <dan.palmer@me.com>
2012-10-12 10:06:23 +0100

    Initial Commit

---
+++ COMP3004/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  COMP3004
+//
+//  Created by Dan Palmer on 12/10/2012.
+//  Copyright (c) 2012 Dan Palmer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
